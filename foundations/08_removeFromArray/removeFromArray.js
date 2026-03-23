dad0c37b93f848c7e409c5d15936653c4c8a10e0
smlnski <163429295+smlnski@users.noreply.github.com>
2026-03-23 15:08:28 +0100

    Task 08: Add solution for first test

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, item) {
+
+    function isNotItem(value) {
+        return function (item) {
+            return value != item;
+        }
+    }
+
+    return array.filter(isNotItem(item));
 };
 
 // Do not edit below this line

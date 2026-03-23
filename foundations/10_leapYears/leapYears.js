8e4f5de38c85042c3271ccfcfa702917dd04fd14
smlnski <163429295+smlnski@users.noreply.github.com>
2026-03-23 16:05:47 +0100

    Add solution for foundations, exercise 10: leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,4 +1,20 @@
-const leapYears = function() {
+const leapYears = function (year) {
+
+    let isLeapYear = false;
+
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                isLeapYear = true;
+            }
+        }
+        else {
+            isLeapYear = true;
+        }
+
+    }
+
+    return isLeapYear;
 
 };
 

b1d59ba7bc9677f7256ddd181ed2b85dd128447e
smlnski <163429295+smlnski@users.noreply.github.com>
2026-03-23 15:49:42 +0100

    Add solution for foundations, exercise 09: sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
+
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+
+    if (a > b) {
+        [a, b] = [b, a];
+    }
+
+    for (int = a; int <= b; int++) {
+        sum += int;
+    }
+
+    return sum;
 
 };
 

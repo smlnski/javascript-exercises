a3871b44dbaaf4ef44886dc2dc55edb0b8a579e0
smlnski <163429295+smlnski@users.noreply.github.com>
2026-03-23 16:31:31 +0100

    Add solution for foundations, exercise 11: tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+
+  const convertedTemp = (temp - 32) / 1.8;
+  return roundToOneDecimal(convertedTemp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+
+  const convertedTemp = temp * 1.8 + 32;
+  return roundToOneDecimal(convertedTemp);
 };
+
+function roundToOneDecimal(value) {
+  return Math.round(value * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
